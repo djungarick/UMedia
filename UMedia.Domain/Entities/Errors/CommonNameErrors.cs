0c83cd963064d191a817f0ad2606eaca7929bfb5
Roman Volkov <roman.volkov.kz@outlook.com>
2024-09-06 23:00:03 +0300

    fix(Server.Domain): fix CommonNameErrors Identifiers

--- UMedia.Domain/Entities/Errors/CommonNameErrors.cs
+++ UMedia.Domain/Entities/Errors/CommonNameErrors.cs
@@ -4,25 +4,25 @@ internal static class CommonNameErrors
 {
     public static readonly ValidationError IsNotUnique = new()
     {
-        Identifier = $"{nameof(CommonNameConstraints)}.{nameof(IsNotUnique)}",
+        Identifier = $"{nameof(CommonNameErrors)}.{nameof(IsNotUnique)}",
         ErrorMessage = "The name should be unique."
     };
 
     public static readonly ValidationError IsNullOrEmpty = new()
     {
-        Identifier = $"{nameof(CommonNameConstraints)}.{nameof(IsNullOrEmpty)}",
+        Identifier = $"{nameof(CommonNameErrors)}.{nameof(IsNullOrEmpty)}",
         ErrorMessage = "The name should not be null or empty."
     };
 
     public static readonly ValidationError StartsWithWhiteSpace = new()
     {
-        Identifier = $"{nameof(CommonNameConstraints)}.{nameof(StartsWithWhiteSpace)}",
+        Identifier = $"{nameof(CommonNameErrors)}.{nameof(StartsWithWhiteSpace)}",
         ErrorMessage = "The name should not start with a white space."
     };
 
     public static readonly ValidationError LengthIsOutOfRange = new()
     {
-        Identifier = $"{nameof(CommonNameConstraints)}.{nameof(LengthIsOutOfRange)}",
+        Identifier = $"{nameof(CommonNameErrors)}.{nameof(LengthIsOutOfRange)}",
         ErrorMessage = $"The length of the name should not be less than {CommonNameConstraints.MinimumLength} or more than {CommonNameConstraints.MaximumLength}."
     };
 }
